9c2acf4ca201f9c3187d0a71fc312d0b95645ee0
Labor <labor@MacBook-Pro-de-Labor.local>
2017-12-01 14:43:43 -0500

    correciones

--- app/components/PostsApp/Post/Post.jsx
+++ app/components/PostsApp/Post/Post.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import style from './style';
 import Comments from './CommentsApp/Comments'
 import { Card, CardTitle, CardText } from 'react-toolbox/lib/card';
-const Post = ({ title, body, date }) => (
-  <Card style={{width: '600px'}}>
-    <CardTitle
-      title={title}
-      subtitle={date}
-    />
-    <CardText>{body}</CardText>
-    <Card>
-      <CardTitle
-        title="Comentarios"
-      />
-        <CardText>
-            <Comments />
-        </CardText>
-    </Card>
-  </Card>
-);
+import { Button } from 'react-toolbox/lib/button';
 
-Post.propTypes = {
-  title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-};
-
+class Post extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }
+  state = {
+    hideComments: true,
+    buttonText: "Mostrar comentarios"
+  }
+  handleToggleComments = () => {
+    if(this.state.hideComments){
+      this.setState({hideComments:false, buttonText:"Ocultar comentarios"});
+    }else{
+      this.setState({hideComments:true, buttonText:"Mostrar comentarios"});
+    }
+  }
+  render(){
+    const { title, date, body } = this.props;
+    return(
+      <Card style={{width: '600px'}}>
+        <CardTitle
+          title={title}
+          subtitle={date}
+        />
+        <CardText>{body}</CardText>
+        <Button
+          icon="show"
+          label={this.state.buttonText}
+          onClick={this.handleToggleComments}
+        />
+        <div hidden={this.state.hideComments}>
+          <Card>
+            <CardTitle
+              title="Comentarios"
+            />
+              <CardText >
+                  <Comments />
+              </CardText>
+          </Card>
+        </div>
+      </Card>
+    )
+  }
+}
 export default Post;
